9ef6e4b306a4fd706c54b8e81d6c381a43295dd3
jdrakes <jdtdrakes@gmail.com>
2016-08-27 15:25:27 -0500

    Code fixes flickrfunctions
    Made flickrfunctions.js code more consistent with inputs needed to
    function properly.

--- public/js/flickrfunctions.js
+++ public/js/flickrfunctions.js
@@ -1,38 +1,17 @@
-    $.get('https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=' + apiKey + '&user_id=' + userId + '&format=json&nojsoncallback=1')
-        .done(function(photos) {
+/*
+    retrieves all photo ids from server via Flickr API
+ */
+function getPhotoIds(defer) {
+    $.get('/photos/ids')
+        .done(function(ids) {
             if (defer !== null && defer !== undefined)
-                defer.resolve(photos.photos.photo);
+                defer.resolve(ids);
             else
-                return photos.photos.photo;
+                return ids;
         })
-        .fail(function(data) {
-            // console.log(data);
-            // console.log('Error');
-            return null;
-        });
-}
-
-function getPhoto(photo, defer) {
-    var urlDefault = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-    // console.log(photo);
-    $('#photoHolder').append('<img src="' + urlDefault + '">');
-    return urlDefault;
-}
-
-function getPhotoSize(apiKey, photo, defer) {
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=' + apiKey + '&photo_id=' + photo.id + '&format=json&nojsoncallback=1';
-    $.get(url)
-        .done(function(sizes) {
-            sizes = sizes.sizes.size;
-            // console.log(sizes);
-            if (defer !== null && defer !== undefined)
-                defer.resolve(sizes);
-            else
-                return sizes;
-        })
-        .fail(function() {
-            // console.log(data);
-            // console.log('Error');
+        .fail(function(error) {
+            errorMessage = JSON.parse(error.responseText);
+            console.log(errorMessage.error);
             if (defer !== null && defer !== undefined)
                 defer.resolve(null);
             else
@@ -40,25 +19,65 @@ function getPhotoSize(apiKey, photo, defer) {
         });
 }
 
-function hasLargePhoto(apiKey, photo, defer) {
-    var deferSize = $.Deferred();
-    var result = null;
-    getPhotoSize(apiKey, photo, deferSize);
-    
-    deferSize.then(function(sizes) {
-        if (sizes !== null && sizes !== undefined) {
-            for (size in sizes) {
-                if (sizes[size].label !== 'Large')
-                    continue;
-                else if (sizes[size].label === 'Large') {
-                    // console.log(sizes[size]);
-                    result = sizes[size];
-                }
-            }
-        }
-        if (defer !== null && defer !== undefined)
-                defer.resolve(result.source);
+/*
+    retrieves photo object from photo database on server by id via Flickr API
+ */
+function getPhoto(photoId, defer) {
+    $.get('/photos/photo/' + photoId)
+        .done(function(photoObj) {
+            if (defer !== null && defer !== undefined)
+                defer.resolve(photoObj);
             else
-                return result.source;
-    });
+                return photoObj;
+        })
+        .fail(function(error) {
+            errorMessage = JSON.parse(error.responseText);
+            console.log(errorMessage.error);
+            if (defer !== null && defer !== undefined)
+                defer.resolve(null);
+            else
+                return null;
+        });
+}
+
+/*
+    retrieves photo default url from photo database on server by id via Flickr API
+ */
+function getPhotoUrl(photoId, defer) {
+    $.get('/photos/photo/' + photoId + '/url')
+        .done(function(url) {
+            if (defer !== null && defer !== undefined)
+                defer.resolve(url);
+            else
+                return url;
+        })
+        .fail(function(error) {
+            errorMessage = JSON.parse(error.responseText);
+            console.log(errorMessage.error);
+            if (defer !== null && defer !== undefined)
+                defer.resolve(null);
+            else
+                return null;
+        });
+}
+
+/*
+    retrieves photo large url from photo database on server by id via Flickr API
+ */
+function getLargePhotoUrl(photoId, defer) {
+    $.get('/photos/photo/' + photoId + '/large')
+        .done(function(url) {
+            if (defer !== null && defer !== undefined)
+                defer.resolve(url);
+            else
+                return url;
+        })
+        .fail(function(error) {
+            errorMessage = JSON.parse(error.responseText);
+            console.log(errorMessage.error);
+            if (defer !== null && defer !== undefined)
+                defer.resolve(null);
+            else
+                return null;
+        });
 }
